71405098dfae7ac27c4f8b46c2e9a0327d908b31
Andres Sandoval <android.sjsu@gmail.com>
2019-04-08 11:20:19 -0700

    first commit

---
+++ app/src/main/java/com/kotlinmap/andres/mapapp/MainActivity.kt
@@ -0,0 +1,159 @@
+package com.kotlinmap.andres.mapapp
+
+import android.content.Context
+import android.content.Intent
+import android.location.Address
+import android.location.Geocoder
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+import com.google.android.gms.maps.model.LatLng
+
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import org.jetbrains.anko.toast
+import java.io.IOException
+import android.content.pm.PackageManager
+import android.util.Log
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Work in Progress", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+            toast("borninapp" + "getAppStore: " + getAppStore(this))
+            Log.v("borninapp", "MainActivity: getAppStore: "+ getAppStore(this))
+
+        }
+
+        btGoToMap.setOnClickListener {
+            //Get location1
+            val locationName = edLocationName.text.toString()
+            val coordinates = getLocationFromAddress(this, locationName)
+
+//            toast("andres " + "lat " + coordinates!!.latitude + ", long :" + coordinates!!.longitude)
+//            if (locationName1 == "") {
+//                toast("setLat(): " + locationName1)
+//            }
+
+            // check if App it's from unknown market; kill app. No app sideload allowed.
+            toast("borninapp" + "getAppStore: " + getAppStore(this))
+            Log.v("borninapp", "MainActivity: getAppStore: "+ getAppStore(this))
+
+
+            //Get location2
+            val locationName2 = edLocationName2.text.toString()
+            val coordinates2 = getLocationFromAddress(this, locationName2)
+
+            //Get location2
+            val locationName3 = edLocationName3.text.toString()
+            val coordinates3 = getLocationFromAddress(this, locationName3)
+
+            // Start map activity
+            val i = Intent(this@MainActivity, MapsActivity::class.java)
+            // i.putExtra("lat", 37.773972)
+            i.putExtra("name", locationName)
+            i.putExtra("lat", coordinates?.latitude.toString())
+            i.putExtra("long", coordinates?.longitude.toString())
+
+            //location 2
+            i.putExtra("name2", locationName2)
+            i.putExtra("lat2", coordinates2?.latitude.toString())
+            i.putExtra("long2", coordinates2?.longitude.toString())
+
+            //location 3
+            i.putExtra("name3", locationName3)
+            i.putExtra("lat3", coordinates3?.latitude.toString())
+            i.putExtra("long3", coordinates3?.longitude.toString())
+
+//            if (locationName == null|| locationName2 == null || locationName3 ==null) {
+            if (!checkTextLength(edLocationName) || !checkTextLength(edLocationName2) || !checkTextLength(edLocationName3))
+                toast("Fields cannot be empty!")
+            else
+                startActivity(i)
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun checkTextLength(editText: EditText): Boolean {
+        var length = editText.length()
+        return length > 0
+    }
+
+    fun getLocationFromAddress(context: Context, inputtedAddress: String): LatLng? {
+
+        val coder = Geocoder(context)
+        val address: List<Address>?
+        var resLatLng: LatLng? = null
+
+        try {
+            // May throw an IOException
+            address = coder.getFromLocationName(inputtedAddress, 5)
+            if (address == null) {
+                return null
+            }
+
+            if (address.isEmpty()) {
+                return null
+            }
+
+            val location = address[0]
+            location.latitude
+            location.longitude
+//            toast("location " + location.getLatitude() + location.getLongitude())
+
+            resLatLng = LatLng(location.latitude, location.longitude)
+
+        } catch (ex: IOException) {
+
+            ex.printStackTrace()
+            toast("error")
+        }
+
+        return resLatLng
+    }
+
+    /**
+     * Get the name of App Store the App is installed from.
+     */
+    fun getAppStore(context: Context): String {
+        val pName = BuildConfig.APPLICATION_ID
+
+        val packageManager = context.packageManager
+        val installPM = packageManager.getInstallerPackageName(pName)
+        if ("com.android.vending" == installPM) {
+            // Installed from the Google Play
+            return "Google Play"
+        } else if ("com.amazon.venezia" == installPM) {
+            // Installed from the Amazon Appstore
+            return "Amazon Appstore"
+        }
+        return "unknown"
+    }
+}
---
+++ app/src/main/java/com/kotlinmap/andres/mapapp/MapsActivity.kt
@@ -0,0 +1,118 @@
+package com.kotlinmap.andres.mapapp
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity;
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+
+import kotlinx.android.synthetic.main.content_maps.*
+import org.jetbrains.anko.toast
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        // 2019 fix https://www.codesd.com/item/kotlin-with-map-in-android.html add ?
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+
+
+        bShare.setOnClickListener {
+            toast("share")
+            val momBornIn = intent.getStringExtra("name")
+            val dadBornIn = intent.getStringExtra("name2")
+            val iWasBornIn = intent.getStringExtra("name3")
+
+            //remove empty space
+            val urlMomBornIn = momBornIn.replace(" ", "_")
+            val urlDadBornIn = dadBornIn.replace(" ", "_")
+            val urlIWasBornIn = iWasBornIn.replace(" ", "_")
+
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Sharing born locations")
+            shareIntent.putExtra(android.content.Intent.EXTRA_TEXT,
+                "Mom born in: " + momBornIn + ", https://en.wikipedia.org/wiki/" + urlMomBornIn
+                        + " Dad born in: " + dadBornIn + ", https://en.wikipedia.org/wiki/" + urlDadBornIn
+                        + " I was born in: " + iWasBornIn + ", https://en.wikipedia.org/wiki/" + urlIWasBornIn
+                        + "\n\n"
+                        + "download our app from this link"
+                        + "https://play.google.com/store/apps/details?id=com.bornin.androidapp")
+            startActivity(Intent.createChooser(shareIntent, "share via"))
+        }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        //location 1
+        //default "Mountain view CA"
+        val name = intent.getStringExtra("name")
+        val lat = intent.getStringExtra("lat").toDouble()
+        val long = intent.getStringExtra("long").toDouble()
+
+        //location 2
+        //default "San Jose CA"
+        val name2 = intent.getStringExtra("name2")
+        val lat2 = intent.getStringExtra("lat2").toDouble()
+        val long2 = intent.getStringExtra("long2").toDouble()
+
+
+        //location 3
+        //default San Francisco CA
+        val name3 = intent.getStringExtra("name3")
+        val lat3 = intent.getStringExtra("lat3").toDouble()
+        val long3 = intent.getStringExtra("long3").toDouble()
+
+        // Add a marker in SF and move the camera
+//        val sF = LatLng(37.773972, -122.431297)
+        val location1 = LatLng(lat, long)
+        val location2 = LatLng(lat2, long2)
+        val location3 = LatLng(lat3, long3)
+
+
+//        mMap.addMarker(MarkerOptions().position(sF).title(name))
+        mMap.addMarker(MarkerOptions().position(location1).title(name))
+        mMap.addMarker(MarkerOptions().position(location2).title(name2))
+        mMap.addMarker(MarkerOptions().position(location3).title(name3))
+
+        toast("andres name $name, name2 :$name2")
+
+
+        //on maeker click opens new activity
+        mMap.setOnMarkerClickListener { marker ->
+            //                val i = Intent(this@MapsActivity, FillinForm::class.java)
+            //                startActivity(i)
+            //                return false //true don't show  marker title
+            val uri = Uri.parse("https://en.wikipedia.org/wiki/" + marker.title)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+            false
+        }
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location1))
+    }
+
+
+}
